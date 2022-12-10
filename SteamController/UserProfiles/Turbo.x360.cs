e26a9214743fd165f9b09eb770a861cba507aebc
baldsealion <93551277+baldsealion@users.noreply.github.com>
2022-12-10 16:00:29 -0500

    Update Turbo.x360.cs

--- SteamController/UserProfiles/Turbo.x360.cs
+++ SteamController/UserProfiles/Turbo.x360.cs
@@ -1,3 +1,9 @@
-// If L5 is hold, the A is turbo: 10x per second
+// If L5 is hold, the A is turbo: 8x per second
+if (Steam.BtnL4)
+    X360.BtnX = Turbo(Steam.BtnX, 8);
 if (Steam.BtnL5)
-    X360.BtnA = Turbo(Steam.BtnA, 10);
+    X360.BtnA = Turbo(Steam.BtnA, 8);
+if (Steam.BtnR4)
+    X360.BtnY = Turbo(Steam.BtnY, 8);
+if (Steam.BtnR5)
+    X360.BtnB = Turbo(Steam.BtnB, 8);
